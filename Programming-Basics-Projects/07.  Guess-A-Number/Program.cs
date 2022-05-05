a908685bbac5495ef371cd2ff5130aeef3ffe267
PetarPaunov <petarp.2000@abv.bg>
2022-05-05 16:09:36 +0300

    Project after Loops

---
+++ Programming-Basics-Projects/07.  Guess-A-Number/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Guess_A_Number
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Random randomNumber = new Random();
+            int computerNumer = randomNumber.Next(1, 101);
+
+            while (true)
+            {
+                Console.Write("Guess a number (1-100): ");
+
+                string playerInput = Console.ReadLine();
+                bool isValid = int.TryParse(playerInput, out int playerNumber);
+
+                Console.Clear();
+
+                if (!isValid)
+                {
+                    Console.WriteLine("Invalid input.");
+                    continue;
+                }
+                else if (playerNumber == computerNumer)
+                {
+                    Console.WriteLine("You guessed it!");
+                    break;
+                }
+                else
+                {
+                    if (playerNumber > computerNumer)
+                    {
+                        Console.WriteLine("High");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Low");
+                    }
+                }
+
+            }
+        }
+    }
+}
