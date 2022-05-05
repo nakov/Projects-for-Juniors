1c9097f3ddfebe87c56a8b4e5ed0c77d7100fffb
PetarPaunov <petarp.2000@abv.bg>
2022-05-05 15:02:57 +0300

    Second Project

---
+++ Programming-Basics-Projects/04. Project-Guess-A-Number/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Guess_A_Number
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Random randomNumber = new Random();
+            int computerNumber = randomNumber.Next(1, 6);
+
+            Console.Write("Guess a number (1-5): ");
+
+            string playerGuese = Console.ReadLine();
+            bool isValid = int.TryParse(playerGuese, out int number);
+
+            if (!isValid)
+            {
+                Console.WriteLine("Invalid input.");
+            }
+            else if (computerNumber != number)
+            {
+                Console.WriteLine($"Incorrect guese.");
+            }
+            else if (computerNumber == number)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+            Console.Write("Press any key to exit...");
+
+
+        }
+    }
+}
